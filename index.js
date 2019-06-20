99a5ab78ab379d99a968d0c08edee0b0c867586f
cdavis <cdavis@tdstickets.com>
2019-06-20 08:00:15 -0400

    API commit 1, just to the point of returning JSON

---
+++ index.js
@@ -0,0 +1,92 @@
+/*
+* Pirples API 
+*/
+
+//dependancies
+const http = require('http');
+const url = require('url');
+const {StringDecoder} = require('string_decoder');
+
+//Server will response setup
+const server = http.createServer((req,res) => {
+
+    //Get url and parse it
+    const parsedURL = url.parse(req.url, true);
+
+    //Get the path
+    const path = parsedURL.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    //Get Query String params as object
+    const queryString = parsedURL.query;
+
+    
+    //Get the HTTP Method
+    const method = req.method.toLowerCase();
+
+    //Get the Headers as an object
+    const headerObject = req.headers
+
+    //Get payload if there is any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+    req.on('end', () => {
+        buffer += decoder.end()
+
+        //Choose the handler the request should go to
+        let choosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        //construct the data object to send to the handler
+        var data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryString,
+            'method' : method,
+            'headers' : headerObject,
+            'payload' : buffer
+        };
+
+        //route the request to the specified handler
+        choosenHandler(data, (statusCode, payload) => {
+            //use the status code called back by the handler or default to 200
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+            //use the payload called back by the handler or default to 
+            payload = typeof(payload) == 'object' ? payload : {};
+
+            //convert the payload to a string
+            const payloadString = JSON.stringify(payload);
+
+            //return the payload
+            res.setHeader('Content-Type', 'application/json')
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            
+            //log the request path
+            console.log("returning this response: ", statusCode, payloadString);
+        });
+    });
+});
+
+//start the server and listen on port 3000
+server.listen(3000, () => {console.log("The Server is listening on Port 3000")});
+
+//defining a handler object
+let handlers = {}
+
+//sample handler
+handlers.sample = (data,callback) => {
+    callback(406, {'name' : 'sample handler'});
+};
+
+//Not Found
+handlers.notFound = (data, callback) => {
+    callback(404);
+};
+
+//defining a request router
+const router = {
+    'sample' : handlers.sample
+};
